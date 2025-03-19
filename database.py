1cc1d4caa29908dcd92c5377301177e917f757f4
Susmitha-7961 <susmitha7961@gmail.com>
2025-03-19 02:25:33 -0400

    Create database.py

---
+++ database.py
@@ -0,0 +1,8 @@
+from typing import Dict, List
+from schemas import Application, Dependency
+
+applications: Dict[int, Application]={}
+dependencies: Dict[str, Dependency]={}
+
+app_counter=1
+dep_counter=1
